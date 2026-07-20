169deb93e7d8d4f4abe57a7fd40a26bbe324c5c9
alikazim1997 <ali14173414@gmail.com>
2026-07-20 08:32:16 +0300

    Update Problem 016 - Unboxing

--- 01-Boxing-and-Unboxing/002-Unboxing/Program.cs
+++ 01-Boxing-and-Unboxing/002-Unboxing/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Unboxing
 {
@@ -10,7 +7,15 @@ namespace Unboxing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("w");
+            int valType = 10;
+            object objType = valType; // Boxing
+
+            // Fixed: unbox from 'objType' instead of 'valType'
+            int unboxedValType = (int)objType;
+
+            Console.WriteLine("Unboxed Value: " + unboxedValType);
+            Console.ReadLine();    
+        
         }
     }
 }
