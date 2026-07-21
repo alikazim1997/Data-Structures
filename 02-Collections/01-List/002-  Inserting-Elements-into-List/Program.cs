01afd3315f18dafdb6460a3f701797e0b2be831e
alikazim1997 <ali14173414@gmail.com>
2026-07-21 08:07:39 +0300

    Add List collection examples

---
+++ 02-Collections/01-List/002-  Inserting-Elements-into-List/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002___Inserting_Elements_into_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> {1,4,5,6,7,8,9,10 };
+            list.Add(11);
+            Console.WriteLine("After adding 11 : "+ string.Join(", ",list));
+            list.Insert(0, 0);
+            Console.WriteLine("After inserting 0 at the beginning  " + string.Join(", ", list));
+            list.InsertRange(2, new List<int> { 2, 3 });
+
+            Console.WriteLine("After adding 11 : " + string.Join(", ", list));
+        }
+    }
+}
---
+++ 02-Collections/01-List/002-  Inserting-Elements-into-List/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("002-  Inserting-Elements-into-List")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("002-  Inserting-Elements-into-List")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("84ad9632-e5b9-4250-84cd-747daad704bb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
