e717c2482368300cad378984ac3b77e287b5d815
alikazim1997 <ali14173414@gmail.com>
2026-07-21 08:12:35 +0300

    Update List insertion example

--- 02-Collections/01-List/002-  Inserting-Elements-into-List/Program.cs
+++ 02-Collections/01-List/002-  Inserting-Elements-into-List/Program.cs
@@ -11,14 +11,27 @@ namespace _002___Inserting_Elements_into_List
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int> {1,4,5,6,7,8,9,10 };
-            list.Add(11);
-            Console.WriteLine("After adding 11 : "+ string.Join(", ",list));
-            list.Insert(0, 0);
-            Console.WriteLine("After inserting 0 at the beginning  " + string.Join(", ", list));
-            list.InsertRange(2, new List<int> { 2, 3 });
+            // List initialization
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            Console.WriteLine("After adding 11 : " + string.Join(", ", list));
+
+            // Adding an element at the end
+            numbers.Add(11);
+            Console.WriteLine("After adding 11: " + string.Join(", ", numbers));
+
+
+            // Inserting an element at a specific position
+            numbers.Insert(0, 0);
+            Console.WriteLine("After inserting 0 at the beginning: " + string.Join(", ", numbers));
+
+
+            // Inserting multiple elements
+            numbers.InsertRange(5, new List<int> { 55, 56 });
+            Console.WriteLine("After inserting 55 and 56 at index 5: " + string.Join(", ", numbers));
+
+
+            // Waiting for a key press
+            Console.ReadKey();
         }
     }
 }
