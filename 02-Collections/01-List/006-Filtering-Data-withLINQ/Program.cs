125a780b0a27f503add75768e54678b4b03bb5d2
alikazim1997 <ali14173414@gmail.com>
2026-07-22 07:47:33 +0300

    Add List collection examples

---
+++ 02-Collections/01-List/006-Filtering-Data-withLINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_Filtering_Data_withLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // List initialization
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Even Numbers: " + string.Join(", ", numbers.Where(n => n % 2 == 0)));
+            Console.WriteLine("Odd Numbers: " + string.Join(",",numbers.Where(n => n % 2!=0)));
+            Console.WriteLine("Numbers Greater Than 5: " + string.Join(", ", numbers.Where(n => n > 5)));
+            Console.WriteLine("Every Second Number: " + string.Join(", ", numbers.Where((n, index) => index % 2 == 1)));
+
+
+
+
+
+
+
+        }
+    }
+}
